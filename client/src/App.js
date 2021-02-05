4733d3b9a946c548c03670b88e4b66a13e192167
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-02-05 14:36:57 -0800

    Update nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-// import those pages in App.js
-// then based on the path show each components using react-router components
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./booking/Home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import TopNav from "./components/TopNav";
+
 
 
 
--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,17 +1,35 @@
+/* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Route } from 'react-router-dom';
 
-const TopNav = () => (
-  <div className="nav bg-light d-flex justify-content-between">
-    <Link className="nav-link" to="/">
-      Home
-    </Link>
-    <Link className="nav-link" to="/login">
-      Login
-    </Link>
-    <Link className="nav-link" to="/register">
-      Register
-    </Link>
-  </div>
-);
+const TopNav = () => {
+  return (
+  <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Home</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/register'>
+                <Nav.Link>
+                  <i className='fa fa-user-plus'></i> Register
+                </Nav.Link>
+              </LinkContainer>
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>               
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+)};
 
 export default TopNav;
