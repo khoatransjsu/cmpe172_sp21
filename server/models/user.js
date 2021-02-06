08b619e0ca12938ef9ba08f29186e4e97287a23f
khoatransjsu <54516445+khoatransjsu@users.noreply.github.com>
2021-02-05 22:47:09 -0800

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -28,21 +28,11 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-/**
- * While saving user, we need to make sure the password is hashed, not plain password
- * hashing should be done only in 2 situations
- * 1. if it is the first time a user is being saved/created
- * 2. user have updated/modified the existing password
- * for handling such requirements, we can use 'pre' middleware in our schema
- * this middleware/function will run each time user is saved/created
- * and/or password is modified/updated
- */
+
 
 userSchema.pre("save", function (next) {
   let user = this;
-  // hash password only if user is changing the password or registering for the first time
-  // make sure to use this otherwise each time user.save() is executed, password
-  // will get auto updated and you can't login with original password
+
   if (user.isModified("password")) {
     return bcrypt.hash(user.password, 12, function (err, hash) {
       if (err) {
