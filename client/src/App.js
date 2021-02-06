3f067d5b556d6c2f4ef3f2e16f8504974a7e363b
khoa <khoatran@Khoas-MacBook-Pro.local>
2021-02-05 17:20:58 -0800

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./booking/Home";
-import Login from "./auth/Login";
-import Register from "./auth/Register";
-import TopNav from "./components/TopNav";
-
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <TopNav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
